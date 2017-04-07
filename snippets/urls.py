1e76d2533c67a34a84ec7aa6a54747168c977691
gknezevic <knezevichgoran@gmail.com>
2017-04-07 15:16:44 +0200

    .

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from snippets import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 router = DefaultRouter()
 router.register(prefix=r'snippets', viewset=views.SnippetViewSet)
@@ -9,6 +12,7 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^schema/$', schema_view),
 ]
 
 
